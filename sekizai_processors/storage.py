6f71dde22d25bd5325e271824265a07174e32efb
Jacob Rief <jacob.rief@gmail.com>
2015-02-15 16:20:45 +0100

    moved utilities into own file

---
+++ sekizai_processors/storage.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.contrib.staticfiles.finders import get_finders
+from django.core.files.storage import FileSystemStorage
+
+
+class SassFileStorage(FileSystemStorage):
+    def __init__(self, location=None, base_url=None, *args, **kwargs):
+        if location is None:
+            location = getattr(settings, 'SEKIZAI_PROCESSORS_ROOT', settings.STATIC_ROOT)
+        if base_url is None:
+            base_url = settings.STATIC_URL
+        super(SassFileStorage, self).__init__(location, base_url, *args, **kwargs)
+
+
+def find_file(path):
+    for finder in get_finders():
+        result = finder.find(path)
+        if result:
+            return result
