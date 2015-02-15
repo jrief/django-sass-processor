91d3e7b9eb79c82789b3f6b82b7799699c783630
Jacob Rief <jacob.rief@gmail.com>
2015-02-15 21:02:04 +0100

    A customized finder for this project

---
+++ sass_processor/finders.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+from django.conf import settings
+from django.contrib.staticfiles.finders import FileSystemFinder
+from django.core.files.storage import FileSystemStorage
+from django.utils.datastructures import SortedDict
+
+
+class CssFinder(FileSystemFinder):
+    """
+    Find static *.css files compiled on the fly using templatetag `{% sass_src "" %}`
+    and stored below `SASS_PROCESSOR_ROOT`.
+    """
+    locations = []
+
+    def __init__(self, apps=None, *args, **kwargs):
+        location = getattr(settings, 'SASS_PROCESSOR_ROOT', settings.STATIC_ROOT)
+        if not os.path.isdir(location):
+            return
+        self.locations = [
+            ('', location),
+        ]
+        self.storages = SortedDict()
+        filesystem_storage = FileSystemStorage(location=location)
+        filesystem_storage.prefix = self.locations[0][0]
+        self.storages[location] = filesystem_storage
+
+    def find(self, path, all=False):
+        if path.endswith('.css') or path.endswith('.css.map'):
+            return super(CssFinder, self).find(path, all)
+        return []
