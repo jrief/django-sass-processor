e3ed6140adcf8c6e4e30f7d216a8b31796280a74
Andreas Backx <andreas@backx.org>
2017-03-04 17:02:37 +0100

    Jinja2 extension now accepts both strings and variables.

--- sass_processor/jinja2/ext.py
+++ sass_processor/jinja2/ext.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from jinja2 import lexer, nodes
+from jinja2 import nodes
 from jinja2.ext import Extension
 from sass_processor.processor import SassProcessor
 
@@ -11,11 +11,7 @@ class SassSrc(Extension):
 
     def parse(self, parser):
         lineno = next(parser.stream).lineno
-
-        token = parser.stream.expect(
-            lexer.TOKEN_STRING
-        )
-        path = nodes.Const(token.value)
+        path = parser.parse_expression()
 
         call = self.call_method(
             '_sass_src_support', [
