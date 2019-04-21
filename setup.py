56df2d760ec135619b6189214c0292691da8b4a2
Jacob Rief <jacob.rief@gmail.com>
2019-04-22 01:29:08 +0200

    use encoding=utf-8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from setuptools import setup, find_packages
 from sass_processor import __version__
 
-with open('README.md', 'r') as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 CLASSIFIERS = [
