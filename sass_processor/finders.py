34924babb7c5fc4c2161bdc0568c3ea16c4e0e4a
Dominik George <nik@naturalnet.de>
2021-03-23 23:55:16 +0100

    finders: Use configured storage

--- sass_processor/finders.py
+++ sass_processor/finders.py
@@ -1,46 +1,18 @@
-import os
-from collections import OrderedDict
-from django.conf import settings
-from django.contrib.staticfiles.finders import FileSystemFinder
-from django.core.exceptions import ImproperlyConfigured
-from django.core.files.storage import FileSystemStorage
+from django.contrib.staticfiles.finders import BaseStorageFinder
+from .storage import SassFileStorage
 
 
-class CssFinder(FileSystemFinder):
+class CssFinder(BaseStorageFinder):
     """
     Find static *.css files compiled on the fly using templatetag `{% sass_src "" %}`
-    and stored below `SASS_PROCESSOR_ROOT`.
+    and stored in configured storage.
     """
-    locations = []
-
-    def __init__(self, apps=None, *args, **kwargs):
-        location = getattr(settings, 'SASS_PROCESSOR_ROOT', settings.STATIC_ROOT)
-        if not location:
-            msg = "Neither 'SASS_PROCESSOR_ROOT' nor 'STATIC_ROOT' has been declared in project settings."
-            raise ImproperlyConfigured(msg)
-        if not os.path.isdir(location):
-            try:
-                location = getattr(settings, 'SASS_PROCESSOR_ROOT')
-                os.makedirs(location)
-            except (AttributeError, OSError):
-                return
-        self.locations = [
-            ('', location),
-        ]
-        self.storages = OrderedDict()
-        filesystem_storage = FileSystemStorage(location=location)
-        filesystem_storage.prefix = self.locations[0][0]
-        self.storages[location] = filesystem_storage
-
-    def find(self, path, all=False):
-        if path.endswith('.css') or path.endswith('.css.map'):
-            return super().find(path, all)
-        return []
+    storage = SassFileStorage()
 
     def list(self, ignore_patterns):
         """
         Do not list the contents of the configured storages, since this has already been done by
-        the other finder of type FileSystemFinder.
+        other finders.
         This prevents the warning ``Found another file with the destination path ...``, while
         issuing ``./manage.py collectstatic``.
         """
