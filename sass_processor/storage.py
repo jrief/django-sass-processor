6154f4dacadfc50c12781180e69be7f6111354dd
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2025-06-17 17:42:25 +0900

    Fixed a bug where ROOT was not being read correctly

--- sass_processor/storage.py
+++ sass_processor/storage.py
@@ -1,3 +1,5 @@
+import copy
+
 from django import VERSION
 from django.conf import settings
 from django.contrib.staticfiles.finders import get_finders
@@ -10,12 +12,14 @@ class SassFileStorage(LazyObject):
     def _setup(self):
         version_parts = VERSION[:2]
         if version_parts[0] > 4 or (version_parts[0] == 4 and version_parts[1] >= 2):
-            staticfiles_storage_backend = settings.STORAGES.get("staticfiles", {}).get("BACKEND")
-            sass_processor_storage = settings.STORAGES.get("sass_processor", {})
+            staticfiles_storage_backend = settings.STORAGES.get('staticfiles', {}).get('BACKEND')
+            sass_processor_storage = settings.STORAGES.get('sass_processor', {})
 
-            storage_path = sass_processor_storage.get("BACKEND") or staticfiles_storage_backend
-            storage_options = sass_processor_storage.get("OPTIONS") or {}
+            storage_path = sass_processor_storage.get('BACKEND') or staticfiles_storage_backend
+            storage_options = copy.deepcopy(sass_processor_storage.get('OPTIONS') or {})
             storage_class = import_string(storage_path)
+
+            storage_options['ROOT'] = sass_processor_storage.get('ROOT') or settings.STATIC_ROOT
         else:
             from django.core.files.storage import get_storage_class
             staticfiles_storage_backend = settings.STATICFILES_STORAGE
@@ -23,10 +27,10 @@ class SassFileStorage(LazyObject):
             storage_options = getattr(settings, 'SASS_PROCESSOR_STORAGE_OPTIONS', {})
             storage_class = get_storage_class(storage_path)
 
-            storage_options["ROOT"] = getattr(settings, 'SASS_PROCESSOR_ROOT', settings.STATIC_ROOT)
+            storage_options['ROOT'] = getattr(settings, 'SASS_PROCESSOR_ROOT', settings.STATIC_ROOT)
 
         if storage_path == staticfiles_storage_backend and issubclass(storage_class, FileSystemStorage):
-            storage_options['location'] = storage_options.pop("ROOT", None) or settings.STATIC_ROOT
+            storage_options['location'] = storage_options.pop('ROOT', None) or settings.STATIC_ROOT
             storage_options['base_url'] = settings.STATIC_URL
 
         self._wrapped = storage_class(**storage_options)
